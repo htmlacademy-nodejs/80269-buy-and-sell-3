789744ac47970c7ab7ef6f7a79e5b7cea56edd02
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-09-18 02:45:40 +0200

    Add utility functions

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const shuffleArray = (array) => {
+  array = array.slice();
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+};
+
+
+module.exports = {
+  getRandomInt,
+  shuffleArray
+};
