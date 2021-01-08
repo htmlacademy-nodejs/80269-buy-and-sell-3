2eee74b3653b7db94dee954a19473c8f2e0e8317
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-01-08 07:33:50 +0200

    2.3. Add --server command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+
 const generate = require(`./generate`);
 const help = require(`./help`);
+const server = require(`./server`);
 const version = require(`./version`);
 
 
 const Cli = {
   [generate.name]: generate,
   [help.name]: help,
+  [server.name]: server,
   [version.name]: version,
 };
 
