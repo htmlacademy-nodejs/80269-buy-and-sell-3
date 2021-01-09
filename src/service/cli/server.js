dd7a4a661b114cf67d6d6c90a1e72702d36bfbaa
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-01-09 09:01:39 +0200

    2.3. Add server response with titles list from mocks.json

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -4,9 +4,11 @@
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const http = require(`http`);
+const HttpCode = require(`../../constants`).HttpCode;
 
 
 const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
 
 
 module.exports = {
@@ -15,8 +17,46 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    const onClientConnect = () => {
+    const sendResponse = (res, statusCode, message) => {
+      const template = `
+        <!Doctype html>
+          <html lang="ru">
+          <head>
+            <title>With love from Node</title>
+          </head>
+          <body>${message}</body>
+        </html>
+      `.trim();
 
+      res.statusCode = statusCode;
+      res.writeHead(statusCode, {
+        'Content-Type': `text/html; charset=UTF-8`,
+      });
+
+      res.end(template);
+    };
+
+    const onClientConnect = async (req, res) => {
+      const notFoundMessage = `Not found!`;
+
+      switch (req.url) {
+        case `/`:
+          try {
+            const fileContent = await fs.readFile(FILENAME);
+            const mocks = JSON.parse(fileContent);
+            const message = mocks
+              .map((post) => `<li>${post.title}</li>`)
+              .join(``);
+            sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+          } catch (err) {
+            sendResponse(res, HttpCode.NOT_FOUND, notFoundMessage);
+          }
+
+          break;
+        default:
+          sendResponse(res, HttpCode.NOT_FOUND, notFoundMessage);
+          break;
+      }
     };
 
     http.createServer(onClientConnect)
