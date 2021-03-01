4b875620e504ae9c130bee5e3bc64dbf8c8915d4
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-01 03:29:45 +0200

    3.2. Add static content. Port changed to 8081 for local testing purposes

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const offerRoutes = require(`./routes/offer-routes.js`);
 const mainRoutes = require(`./routes/main-routes.js`);
 const myRoutes = require(`./routes/my-routes.js`);
 
 
-const DEFAULT_PORT = 8080;
+const DEFAULT_PORT = 8081;
+const PUBLIC_DIR = `public`;
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/offers`, offerRoutes);
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
