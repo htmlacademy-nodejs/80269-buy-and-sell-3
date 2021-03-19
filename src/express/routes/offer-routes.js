534086fee33c55f057e6aaf05aad317fa692deb4
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2021-03-19 07:24:24 +0200

    3.2. Add category template and fix resource pathes

--- src/express/routes/offer-routes.js
+++ src/express/routes/offer-routes.js
@@ -6,7 +6,8 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => {
-  res.send(`/offers/category/${req.params.id}`);
+  res.render(`category`);
+  // res.send(`/offers/category/${req.params.id}`);
 });
 offersRouter.get(`/add`, (req, res) => {
   res.send(`/offers/add`);
